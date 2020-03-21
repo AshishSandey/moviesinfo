6ee9bbf50c7b19ecb2429b8eaec89cce3cede002
Xplorer123 <ashishsandey5@gmail.com>
2020-03-21 18:01:19 +0530

    Updates

---
+++ precache-manifest.e9ee966b268bfa6c9a5c1901acb9fc4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ba988b1c6562d570d570c4f99ca844b",
+    "url": "/github.io/moviesinfo/index.html"
+  },
+  {
+    "revision": "319c8e6deca00abd0b25",
+    "url": "/github.io/moviesinfo/static/css/main.6bf802cf.chunk.css"
+  },
+  {
+    "revision": "fa86de4771c66265288e",
+    "url": "/github.io/moviesinfo/static/js/2.d0e19e5b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/github.io/moviesinfo/static/js/2.d0e19e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "319c8e6deca00abd0b25",
+    "url": "/github.io/moviesinfo/static/js/main.f5095360.chunk.js"
+  },
+  {
+    "revision": "da088fd1f2ce2d482b39",
+    "url": "/github.io/moviesinfo/static/js/runtime-main.b8a5f5bd.js"
+  },
+  {
+    "revision": "5c7392a91f8cd5d997137f6b4a36870b",
+    "url": "/github.io/moviesinfo/static/media/redux-logo.5c7392a9.svg"
+  }
+]);
